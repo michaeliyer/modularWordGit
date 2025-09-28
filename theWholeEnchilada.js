fda3be06df95888977278d14bde8ed86a5e92810
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-09-28 18:33:35 -0400

    Gooey dealt with, for real

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -12121,7 +12121,7 @@ console.log("Push, Stash, and Pop. What the Chryste!")
 console.log("August 4, 2025, Jackwagon. Dickerson. Eff On Off Guyo.")
 console.log("August 7, 2025, CORAL hits! Now what? Scion? Rinse? Caste? But CORAL HITS!")
 console.log("August 9, 2025, Mom: Hello" )
-console.log("September 28, 2025: Trump sux dykk0000! GOOEY")
+console.log("September 28, 2025: Trump sux dykk000000000! GOOEY")
 
 
 console.log(wordleWords)
@@ -12141,3 +12141,4 @@ function logOverallAverageScore() {
 }
 
 logOverallAverageScore();
+
